3cbed03b70f9f49c339d3dd04be32bafff3016ca
Christian <cackermann@kunder.cl>
2019-08-06 14:30:42 -0400

    noImplicitThis

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,6 @@
-class MyClass {
-    myMethod() {
-        const foo = 123
-        console.log('1', this)
-        // a function creates new scope whereas an arrow function doesnt bind a "this" value
-        setTimeout(() => {
-            console.log('2', this)
-        }, 0)
-    }
-
+const elem = document.querySelector('.click')
+function handleClick(this: HTMLAnchorElement, event: Event) {
+    event.preventDefault();
+    console.log(this.className)
 }
-const myInstance = new MyClass()
-myInstance.myMethod()
+elem.addEventListener('click', handleClick, false)

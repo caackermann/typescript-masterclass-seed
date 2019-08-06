6ba912da17c55160ecab463b8cdcba7a06a99d88
Christian <cackermann@kunder.cl>
2019-08-06 14:38:50 -0400

    typed queries explained

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,14 @@ const person = {
     name: 'Todd',
     age: 27
 }
+
+//creates a new type for person
 type Person = typeof person
-//In JavaScript, typeof person = 'object'
+
+//binding to another value
 const anotherPerson: Person = {
     name: 'John',
     age: 30,
 }
+
+//In JavaScript, typeof person = 'object'
